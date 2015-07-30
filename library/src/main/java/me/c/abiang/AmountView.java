b3c12313fe8319de41ca9d622a071146944fa991
C <c@littlemusic.tv>
2015-07-30 19:17:31 +0800

    build

---
+++ library/src/main/java/me/c/abiang/AmountView.java
@@ -0,0 +1,102 @@
+package me.c.abiang;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.util.AttributeSet;
+import android.util.TypedValue;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import me.c.abiang.library.R;
+
+/**
+ * 自定义组件：购买数量，带减少增加按钮
+ * Created by C on 2015/7/22.
+ */
+public class AmountView extends LinearLayout implements View.OnClickListener {
+
+    private int amount = 1; //购买数量
+    private int goods_storage = 1; //商品库存
+
+    private OnAmountChangeListener mListener;
+
+    private TextView tvAmount;
+    private Button btnDecrease;
+    private Button btnIncrease;
+
+    public AmountView(Context context) {
+        this(context, null);
+    }
+
+    public AmountView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        LayoutInflater.from(context).inflate(R.layout.view_amount, this);
+        tvAmount = (TextView) findViewById(R.id.tvAmount);
+        btnDecrease = (Button) findViewById(R.id.btnDecrease);
+        btnIncrease = (Button) findViewById(R.id.btnIncrease);
+        btnDecrease.setOnClickListener(this);
+        btnIncrease.setOnClickListener(this);
+
+        TypedArray obtainStyledAttributes = getContext().obtainStyledAttributes(attrs, R.styleable.AmountView);
+        int btnWidth = obtainStyledAttributes.getDimensionPixelSize(R.styleable.AmountView_btnWidth, LayoutParams.WRAP_CONTENT);
+        int tvWidth = obtainStyledAttributes.getDimensionPixelSize(R.styleable.AmountView_tvWidth, 80);
+        int tvTextSize = obtainStyledAttributes.getDimensionPixelSize(R.styleable.AmountView_tvTextSize, 0);
+        int btnTextSize = obtainStyledAttributes.getDimensionPixelSize(R.styleable.AmountView_btnTextSize, 0);
+
+        LayoutParams btnParams = new LayoutParams(btnWidth, LayoutParams.MATCH_PARENT);
+        btnDecrease.setLayoutParams(btnParams);
+        btnIncrease.setLayoutParams(btnParams);
+        if (btnTextSize != 0) {
+            btnDecrease.setTextSize(TypedValue.COMPLEX_UNIT_PX, btnTextSize);
+            btnIncrease.setTextSize(TypedValue.COMPLEX_UNIT_PX, btnTextSize);
+        }
+
+        LayoutParams textParams = new LayoutParams(tvWidth, LayoutParams.MATCH_PARENT);
+        tvAmount.setLayoutParams(textParams);
+        if (tvTextSize != 0) {
+            tvAmount.setTextSize(tvTextSize);
+        }
+
+        obtainStyledAttributes.recycle();
+    }
+
+    public void setListener(Object object){
+        this.mListener = (OnAmountChangeListener) object;
+    }
+
+    public void setGoods_storage(int goods_storage) {
+        this.goods_storage = goods_storage;
+    }
+
+    public int getGoods_storage() {
+        return goods_storage;
+    }
+
+    @Override
+    public void onClick(View v) {
+        int i = v.getId();
+        if (i == R.id.btnDecrease) {
+            if (amount > 1) {
+                amount--;
+                tvAmount.setText(amount + "");
+            }
+        } else if (i == R.id.btnIncrease) {
+            if (amount < goods_storage) {
+                amount++;
+                tvAmount.setText(amount + "");
+            }
+        }
+
+        mListener.onAmountChange(amount);
+    }
+
+
+    public interface OnAmountChangeListener{
+        void onAmountChange(int amount);
+    }
+
+}
